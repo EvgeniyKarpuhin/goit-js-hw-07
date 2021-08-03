a07d6ce22e0acb0e704148a3d7cd88ece7345704
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2021-08-03 18:30:52 +0300

    Create task-02.js

---
+++ js/task-02.js
@@ -0,0 +1,12 @@
+const ingredients = [
+    'Картошка',
+    'Грибы',
+    'Чеснок',
+    'Помидоры',
+    'Зелень',
+    'Приправы',
+  ];
+
+  const addingredients = (array, selector) => {
+      
+  }
