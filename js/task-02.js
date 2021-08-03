75cc6c64377bc41a3be6bb483fe7c956b7757912
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2021-08-03 21:03:30 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -11,7 +11,7 @@ const ingredients = [
       array.map(item => {
           const list = document.createElement('li');
           list.textContent = item;
-          return item;
+          return list;
       });
 
       return document.querySelector(selector).append(...array);
