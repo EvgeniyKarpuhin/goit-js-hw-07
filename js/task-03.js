8fe9e7d594055f40a5d7a6d4c3693013b0acd699
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2021-08-04 12:24:22 +0300

    Update task-03.js

---
+++ js/task-03.js
@@ -0,0 +1,50 @@
+const images = [
+    {
+      url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      alt: 'White and Black Long Fur Cat',
+    },
+    {
+      url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    },
+    {
+      url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      alt: 'Group of Horses Running',
+    },
+  ];
+  
+//   const addList = document.querySelector('#gallery');
+
+//   const image = images[0];
+//   const addImages = images.map(image => {
+//     const newList = document.createElement('li');
+//     newList.classList = 'gallery__img';
+
+// const imgNew = document.createElement('img');
+// imgNew.src = image.url;
+// imgNew.alt = image.alt;
+// imgNew.width = 320;
+
+// newList.appendChild(imgNew);
+
+// addList.append(newList);
+//   });
+
+function createList(images) {
+    const addList = document.querySelector('#gallery');
+    // addList.classList.add('gallery__img');
+    
+    const imgNew = images.reduce(
+      (image, item) => 
+      image + `<li>
+      <img class = "gallery__img" 
+      src = "${item.url}"
+      alt = "${item.alt}" 
+      width = 320 >
+      </li>`,
+      ""
+    );
+    
+    addList.insertAdjacentHTML('afterbegin', imgNew);  
+  };
+  makeGallery(images);
