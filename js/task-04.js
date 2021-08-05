afb6a11493399ee8892a4b094b25794aedea1358
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2021-08-05 23:45:48 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -3,13 +3,15 @@ const valueId = document.querySelector('#value');
 const decrement = number.firstElementChild;
 const increment = number.lastElementChild;
 
+decrement.addEventListener('click', decrem);
+increment.addEventListener('click', increm);
+
 function decrem() {
     valueId.textContent -= 1;
 }
 
 function increm () {
-    valueId.textContent +=1;
+    // valueId.textContent +=1;
+    valueId.textContent = Number(valueId.textContent) + 1;
 }
 
-decrement.addEventListener('click', decrem);
-increment.addEventListener('click', increm);
