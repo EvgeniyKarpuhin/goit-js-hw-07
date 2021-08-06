c6732e873543435ea8da5386b857c1939bda2555
EvgeniyKarpuhin <evgeniygalaxy4@gmail.com>
2021-08-06 17:50:57 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,4 +1,4 @@
 function text(value) {
-    document.getElementById('font-size-control').style.transform = "fontsize(" + value + "px)";
+    document.getElementById('font-size-control').style.transform.fontSize = "fontSize(" + value + "px)";
     document.getElementById('text').innerHTML = value + "px";
 }
